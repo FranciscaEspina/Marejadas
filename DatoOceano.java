16f3e852ae84748e98804fdd7267a9b3cb5fc177
Fran <fr.espinah@duocuc.cl>
2025-09-29 20:43:22 -0300

    Add files via upload

---
+++ DatoOceano.java
@@ -0,0 +1,76 @@
+package marejadas;
+
+public class DatoOceano extends Dato{
+    private double hm0; //Altura media de las olas en metros
+    private double tz; //Periodo medio de la ola en segundos
+    private double te; //Periodo energético de la ola en segundos
+    
+    public DatoOceano(double anio, double mes, double dia, double hora, double hm0, double tz, double te){
+        super(anio, mes,dia,hora);
+        this.hm0 = hm0;
+        this.tz = tz;
+        this.te = te;
+    }
+    
+    //Polimorfismo
+    @Override
+    public void mostrar(){
+        System.out.println("Fecha: "+ anio + "-" + mes + "-" + dia +" "+ hora+ "h"+" | Hm0=" + getHm0() + " | Tz="+ getTz()+ " |Te="+getTe());
+    }
+
+    /**
+     * @return the hm0
+     */
+    public double getHm0() {
+        return hm0;
+    }
+
+    /**
+     * @param hm0 the hm0 to set
+     */
+    public void setHm0(double hm0) {
+        this.hm0 = hm0;
+    }
+
+    /**
+     * @return the tz
+     */
+    public double getTz() {
+        return tz;
+    }
+
+    /**
+     * @param tz the tz to set
+     */
+    public void setTz(double tz) {
+        this.tz = tz;
+    }
+
+    /**
+     * @return the te
+     */
+    public double getTe() {
+        return te;
+    }
+
+    /**
+     * @param te the te to set
+     */
+    public void setTe(double te) {
+        this.te = te;
+    }
+    
+    public double getAnio(){
+        return anio;
+    }
+    public double getMes(){
+        return mes;
+        
+    }
+    public double getDia(){
+        return dia;
+    }
+    public double getHora(){
+        return hora;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,45 @@
+package marejadas;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        //LectorArchivo lector = new LectorArchivo();
+        //List<Dato> datos = lector.leer("C:\\Users\\Duoc\\Downloads\\NodoIquique.txt");
+        
+        
+        //for(Dato d : datos){
+        //    d.mostrar();
+        //}
+        //***Consulta directa al sitio web
+        try{
+            //1.- Definir la URL
+            String urlStr = "https://fundacion-instituto-profesional-duoc-uc.github.io/ATY1102-Surf/Nodo%2010%20(-37,-75)%20-%20Talcahuano.txt";
+            var url = new URL(urlStr);
+            //"2.- Crear un archivo temporal
+            File tempFile = File.createTempFile("NodoIquique",".txt");
+            tempFile.deleteOnExit();
+            //3.- Copiar desde la URL el archivoi temporal
+            try(InputStream in = url.openStream()){
+                Files.copy(in, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+            //4.- Unificar el archivo temporal con los objetos 
+            LectorArchivo lector = new LectorArchivo();
+            List<Dato> datos = lector.leer(tempFile.getAbsolutePath());
+            //5.- mostrar resultados
+            for(Dato d : datos){
+            d.mostrar();
+            }
+            
+            //6.- Ola más alta
+            AnalizadorMarejadas analizador = new AnalizadorMarejadas(datos);
+            analizador.olaMasAlta();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
